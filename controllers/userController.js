ba2006ca7212e32bc458c7087b7211f000f0eedd
rayhanmfahrezi <rayhan.mfahrezi19@gmail.com>
2026-01-12 01:30:11 +0700

    add getUserByUsername in User

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+const transporter = require("../utils/mailer");
+const generatePassword = require("../utils/generatePassword");
 
 // READ ALL
 exports.getUsers = async (req, res) => {
@@ -7,6 +10,38 @@ exports.getUsers = async (req, res) => {
       'SELECT * FROM "User" ORDER BY created_at DESC'
     );
     res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  const { id } = req.params;
+  try { 
+    const result = await pool.query(
+      'SELECT * FROM "User" WHERE id = $1',
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getUserByUsername = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const result = await pool.query(
+      'SELECT * FROM "User" WHERE username = $1',
+      [username]
+    );  
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(result.rows[0]);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
 const {
   getUsers,
+  getUserByUsername,
 } = require('../controllers/userController');
 
 router.get('/', verifyToken, getUsers);
+router.get('/:username', verifyToken, getUserByUsername);
 
 module.exports = router;
