0b7d8247f7986c8e957d4be851cf85d716b477e8
Fnhafiz <farhanhafiz55@gmail.com>
2026-01-13 22:10:39 +0700

    feat: add retieve payment with va

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { verifyToken } = require('../middleware/authMiddleware');
+const { verifyToken } = require("../middleware/authMiddleware");
 const {
   createPayment,
   getPayments,
   getPaymentById,
   updatePaymentStatus,
   deletePayment,
-  retrievePaymentPeriod
-} = require('../controllers/paymentController');
+  retrievePaymentPeriod,
+  retrievePaymentAndVAPeriod,
+} = require("../controllers/paymentController");
 
-router.post('/', verifyToken, createPayment);
-router.get('/', verifyToken, getPayments);
-router.get('/:id', verifyToken, getPaymentById);
-router.put('/:id', verifyToken, updatePaymentStatus);
-router.delete('/:id', verifyToken, deletePayment);
-router.post('/retrieve', verifyToken, retrievePaymentPeriod)
+router.post("/", verifyToken, createPayment);
+router.get("/", verifyToken, getPayments);
+router.get("/:id", verifyToken, getPaymentById);
+router.put("/:id", verifyToken, updatePaymentStatus);
+router.delete("/:id", verifyToken, deletePayment);
+router.post("/retrieve", verifyToken, retrievePaymentPeriod);
+router.post("/retrieve-with-va", verifyToken, retrievePaymentAndVAPeriod);
 
 module.exports = router;
